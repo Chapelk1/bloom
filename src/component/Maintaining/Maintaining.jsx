f77fb6ab78c7ba4f56a152ab77ad9f27a8538e7c
Chapelk1 <klepach154789@gmail.com>
2024-04-01 19:17:22 +0300

    add swiper

--- src/component/Maintaining/Maintaining.jsx
+++ src/component/Maintaining/Maintaining.jsx
@@ -1,9 +1,37 @@
-import { Title, Wrap } from "./Maintaining.styled";
+import {
+  Title,
+  WrapText,
+  SubTitle,
+  Posiition,
+  Descr,
+  Text,
+  Logo,
+  WrapPhoto,
+  Photo,
+} from "./Maintaining.styled";
 import { Section, Container } from "component/Helpers/Helpers.styled";
-import { register } from "swiper/element/bundle";
-register();
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, EffectFade } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import "./style-swiper.css";
+import vivienne from "assets/vivienne.jpg";
+import img from "assets/IMAGE-07.jpg";
+import logo from "assets/bumble.png";
 function Maintaining() {
+  const pagination = {
+    clickable: true,
+    renderBullet: (index, className) => {
+      switch (index) {
+        case 0:
+          return '<span class="' + className + '">' + "Product" + "</span>";
+
+        case 1:
+          return '<span class="' + className + '">' + "Engineering" + "</span>";
+      }
+    },
+  };
+
   return (
     <Section style={{ padding: "161px 0 0 0" }}>
       <Container>
@@ -11,35 +39,49 @@ function Maintaining() {
           bloom support and enable early stage startups, scaleups and leading
           global tech giants to build and nurture high-performing
         </Title>
-        <Wrap>
-          <swiper-container
-            navigation="true"
-            pagination="true"
-            scrollbar="true"
-            style={{width: '100%', height: '500px'}}
-          >
-            <swiper-slide>11111111111</swiper-slide>
-            <swiper-slide>22222222222</swiper-slide>
-            <swiper-slide>33333333333</swiper-slide>
-          </swiper-container>
 
-          {/* <div className="swiper">
-            <div className="card">
-              <div>
-                <h3>Vivienne Sung</h3>
-                <p>VP Product</p>
-                <p>Mum of 2, Peloton junkie and bibliophile</p>
-                <p>
-                  Sam worked across a mix of Senior and Staff product manager
-                  roles, bringing in extremely high quality candidates across
-                  the board, several of which we hired. Sam was an absolute
-                </p>
-                <img src="" alt="" />
-              </div>
-              <img src="" alt="" />
-            </div>
-          </div> */}
-        </Wrap>
+        <Swiper
+          effect={"fade"}
+          // spaceBetween={50}
+          pagination={pagination}
+          modules={[Pagination, EffectFade]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <WrapText>
+              <SubTitle>Vivienne Sung</SubTitle>
+              <Posiition>VP Product</Posiition>
+              <Descr>Mum of 2, Peloton junkie and bibliophile</Descr>
+              <Text>
+                Sam worked across a mix of Senior and Staff product manager
+                roles, bringing in extremely high quality candidates across the
+                board, several of which we hired. Sam was an absolute
+              </Text>
+              <Logo src={logo} alt="bumble" />
+            </WrapText>
+            <WrapPhoto>
+              <Photo src={vivienne} alt="person" />
+            </WrapPhoto>
+          </SwiperSlide>
+          <SwiperSlide>
+            <WrapText>
+              <SubTitle>Lorem, ipsum.</SubTitle>
+              <Posiition>Lorem, ipsum dolor.</Posiition>
+              <Descr>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              </Descr>
+              <Text>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore
+                voluptatem aspernatur sed amet magni! Repellat ducimus dolorum
+                consequatur ex expedita!
+              </Text>
+              <Logo src={logo} alt="bumble" />
+            </WrapText>
+            <WrapPhoto>
+              <Photo src={img} alt="person" />
+            </WrapPhoto>
+          </SwiperSlide>
+        </Swiper>
       </Container>
     </Section>
   );
--- src/component/Maintaining/Maintaining.styled.js
+++ src/component/Maintaining/Maintaining.styled.js
@@ -10,9 +10,50 @@ export const Title = styled.h2`
   margin-bottom: 101px;
 `;
 
-export const Wrap = styled.div`
-  border-radius: 32px;
-  width: 1216px;
-  height: 562px;
-  background: #f8f8f6;
+export const WrapText = styled.div`
+  max-width: 706px;
+  text-align: start;
+  align-self: flex-end;
 `;
+export const SubTitle = styled.h3`
+  font-family: var(--font-family);
+  font-weight: 400;
+  font-size: 34px;
+  line-height: 1.3;
+  color: var(--text-color-second);
+`;
+export const Posiition = styled.p`
+  font-family: var(--font-family);
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 1.5;
+  color: var(--text-color-second);
+`;
+export const Descr = styled.p`
+  font-family: var(--second-family);
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 1.5;
+  color: var(--text-color-second);
+  margin-bottom: 33px;
+`;
+export const Text = styled.p`
+  font-family: var(--font-family);
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 1.3;
+  color: var(--text-color-second);
+  margin-bottom: 80px;
+`;
+export const Logo = styled.img`
+  width: 149px;
+  height: auto;
+`;
+export const WrapPhoto = styled.div`
+  border-radius: 6px;
+  padding: 14px 14px 54px 14px;
+  align-self: center;
+  box-shadow: -5px 3px 6px 0 rgba(0, 0, 0, 0.15);
+  background: #fff;
+`;
+export const Photo = styled.img``;
