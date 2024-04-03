af470151f7db7ca5f6b5f0925002777223e73c7b
Chapelk1 <klepach154789@gmail.com>
2024-04-03 23:03:10 +0300

    partnerSwiper

--- src/component/Partner/Partner.jsx
+++ src/component/Partner/Partner.jsx
@@ -1,15 +1,63 @@
+import { Section, Container } from "component/Helpers/Helpers.styled";
+import {
+  Wrap,
+  Name,
+  Position,
+  Descr,
+  Text,
+  Logo,
+  Arrow,
+} from "./Partner.styled";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/effect-cards";
+import "./style.css";
+import { EffectCards } from "swiper/modules";
+import logo from "assets/img/monzo.png";
+import arrow from "assets/img/arrow-bottom-1.png";
 function Partner() {
   return (
-    <section>
-      <img src="" alt="" />
-      <div>
-        <h2></h2>
-        <p></p>
-        <p></p>
-        <p></p>
-        <img src="" alt="" />
-      </div>
-    </section>
+    <Section style={{ paddingTop: "166px", paddingBottom: "57px" }}>
+      <Container
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          paddingLeft: "70px",
+        }}
+      >
+        <Swiper
+          loop={true}
+          effect={"cards"}
+          grabCursor={true}
+          modules={[EffectCards]}
+          className="partnerSwiper"
+          cardsEffect={{
+            perSlideOffset: 8,
+            rotate: false,
+          }}
+        >
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+        </Swiper>
+        <Wrap>
+          <Name>Dave Richardson</Name>
+          <Position>Director of Recruitment</Position>
+          <Descr>Collector of Vinyls & Exterior Decorator</Descr>
+          <Text>
+            I've partnered with bloom twice, at Deliveroo and Monzo, & both
+            times they have been really successful. Partnering with bloom is
+            effortless, seamless and most importantly{" "}
+            <span>fun, they just get it !</span>
+          </Text>
+          <Logo src={logo} alt="" />
+          <Arrow src={arrow} alt="arrow" />
+        </Wrap>
+      </Container>
+    </Section>
   );
 }
 
---
+++ src/component/Partner/Partner.styled.js
@@ -0,0 +1,46 @@
+import styled from "@emotion/styled";
+
+export const Wrap = styled.div`
+  position: relative;
+`;
+export const Name = styled.h2`
+  font-family: var(--font-family);
+  font-weight: 400;
+  font-size: 34px;
+  line-height: 1.3;
+  color: var(--text-color-second);
+`;
+export const Position = styled.p`
+  font-family: var(--font-family);
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 1.5;
+  color: var(--text-color-second);
+`;
+export const Descr = styled.p`
+  font-family: var(--second-family);
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 1.5;
+  color: var(--text-color-second);
+  margin-bottom: 33px;
+`;
+export const Text = styled.p`
+  font-family: var(--font-family);
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 1.3;
+  color: var(--text-color-second);
+  max-width: 794px;
+  margin-bottom: 33px;
+
+  span {
+    display: block;
+  }
+`;
+export const Logo = styled.img``;
+export const Arrow = styled.img`
+  position: absolute;
+  right: 68px;
+  bottom: -161px;
+`;
